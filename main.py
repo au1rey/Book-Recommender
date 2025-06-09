012caede19267f78dc186f5a66d756e3953c5dbb
au1rey <saidelaudrey@gmail.com>
2025-06-09 13:22:43 -0400

    added routes

--- main.py
+++ main.py
@@ -11,5 +11,32 @@ def home():
         books = fetch_books_by_query(query)
     return render_template('home.html', books=books)
 
+@app.route('/base')
+def base():
+    render_template('basse.html')
+
+@app.route('/recme')
+def rec_me():
+    render_template('recme.html')
+
+@app.route('/profile')
+def profile():
+    render_template('profile.html')
+
+@app.route('/library')
+def library():
+    rendertemplate("library.html")
+
+@app.route('/shelf/<shelfname>')
+def display_shelf(shelf_name):
+    all_shelves = {
+        "read": [],
+        "to read": [],
+        "in progress": []
+    }
+
+    books = all_shelves.get(shelf_name.lower(), [])
+    return render_template("shelf.html", shelf_name=shelf_name.title(), books=books)
+
 if __name__ == "__main__":
     app.run(debug=True)
